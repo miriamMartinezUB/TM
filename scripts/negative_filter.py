39f39a529e2c3818f35fa1e7904d450685605167
marcLirola <mlirolto51@alumnes.ub.edu>
2024-04-17 17:22:50 +0200

    first idea negative filter

---
+++ scripts/negative_filter.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+from skimage import io, util
+def negative_filter(image_path):
+    image = io.imread(image_path, as_gray=True)
+
+    # Apply the negative filter
+    negative_image = util.invert(image)
+
+    # Display the original and negative images
+    plt.subplot(1, 2, 1)
+    plt.imshow(image, cmap='gray')
+    plt.title('Original Image')
+    plt.axis('off')
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(negative_image, cmap='gray')
+    plt.title('Negative Image')
+    plt.axis('off')
+
+    plt.show()
