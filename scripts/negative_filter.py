ca414ce818cbcddbad932144692fcb641956359f
marcLirola <mlirolto51@alumnes.ub.edu>
2024-05-03 20:41:56 +0200

    negative filter

--- scripts/negative_filter.py
+++ scripts/negative_filter.py
@@ -1,7 +1,14 @@
 from PIL import Image
-from skimage import util
+import numpy as np
+
 def negative_filter(images):
     negative_images = []
     for image in images:
-        negative_images.append(util.invert(image))
+        # Convert PIL image to numpy array
+        image_np = np.array(image)
+        # Invert the colors
+        inverted_image_np = 255 - image_np
+        # Convert back to PIL image
+        inverted_image = Image.fromarray(inverted_image_np)
+        negative_images.append(inverted_image)
     return negative_images
