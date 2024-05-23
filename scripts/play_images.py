0203347f11f8295d03cdfa38bc5cb5d8e0829f68
marcLirola <mlirolto51@alumnes.ub.edu>
2024-05-23 23:25:51 +0200

    añadido progress bar y fps a la reproducción de video

--- scripts/play_images.py
+++ scripts/play_images.py
@@ -4,23 +4,31 @@ from tqdm import tqdm
 import numpy as np
 
 def show_images_as_video(image_path, fps, cmap):
-    fig, ax = plt.subplots()
+  '''
+  This mehtod gets a group of images and reproduces a video at the specified fps
 
-    if cmap == 'gray':
-        image_display = ax.imshow(image_path[0], cmap='gray')
-    else:
-        image_display = ax.imshow(image_path[0])
+  :param image_path: path where the frames are located
+  :param fps: number of fps we want to display the video
+  :param cmap: it says if image are grayscale or not
 
-    interval = 1000 / fps
-    progress_bar = tqdm(total=len(image_path), desc="Processing frames", unit="frame")
+  '''
+  fig, ax = plt.subplots()
 
-    def update(frame):
-        image_display.set_array(image_path[frame])
-        progress_bar.update(1)
-        if frame == len(image_path) - 1:
-            progress_bar.reset()
+  if cmap == 'gray':
+      image_display = ax.imshow(image_path[0], cmap='gray')
+  else:
+      image_display = ax.imshow(image_path[0])
 
-    ani = animation.FuncAnimation(fig, update, frames=len(image_path), interval=interval, repeat=True)
-    plt.axis('off')
-    plt.show()
-    progress_bar.close()
+  interval = 1000 / fps
+  progress_bar = tqdm(total=len(image_path), desc="Processing frames", unit="frame")
+
+  def update(frame):
+      image_display.set_array(image_path[frame])
+      progress_bar.update(1)
+      if frame == len(image_path) - 1:
+          progress_bar.reset()
+
+  ani = animation.FuncAnimation(fig, update, frames=len(image_path), interval=interval, repeat=True)
+  plt.axis('off')
+  plt.show()
+  progress_bar.close()
