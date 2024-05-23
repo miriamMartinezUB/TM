4a6a073cfef4bf0b75c6c86af9b6a7166ad80b67
marcLirola <mlirolto51@alumnes.ub.edu>
2024-05-23 18:53:10 +0200

    añadido progress bar y fps a la reproducción de video

--- scripts/play_images.py
+++ scripts/play_images.py
@@ -1,17 +1,26 @@
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
-
+from tqdm import tqdm
+import numpy as np
 
 def show_images_as_video(image_path, fps, cmap):
-    fig = plt.figure()
+    fig, ax = plt.subplots()
 
-    ims = []
-    for image in image_path:
-        if cmap == 'gray':
-            ims.append([plt.imshow(image, cmap='gray', animated=True)])
-        else:
-            ims.append([plt.imshow(image, animated=True)])
+    if cmap == 'gray':
+        image_display = ax.imshow(image_path[0], cmap='gray')
+    else:
+        image_display = ax.imshow(image_path[0])
+
     interval = 1000 / fps
-    ani = animation.ArtistAnimation(fig, ims, interval=interval, blit=True, repeat_delay=0)
+    progress_bar = tqdm(total=len(image_path), desc="Processing frames", unit="frame")
+
+    def update(frame):
+        image_display.set_array(image_path[frame])
+        progress_bar.update(1)
+        if frame == len(image_path) - 1:
+            progress_bar.reset()
+
+    ani = animation.FuncAnimation(fig, update, frames=len(image_path), interval=interval, repeat=True)
     plt.axis('off')
     plt.show()
+    progress_bar.close()
